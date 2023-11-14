75835a5c8a62e187fe1d6a970657b6955e5d1b5d
Igor Ignatev <igor.ignatev@skyselect.com>
2023-11-14 16:29:49 +0200

    typo2

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ fi
 echo "executor.json"
 echo '{"name":"GitHub Actions", "type":"github", "reportName":"Allure Report with history",' > executor.json
 echo "\"url\":\"${S3_WEBSITE_URL}\"," >> executor.json # ???
-echo "\"reportUrl\":\"{S3_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}\"," >> executor.json
+echo "\"reportUrl\":\"${S3_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}\"," >> executor.json
 echo "\"buildUrl\":\"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}\"," >> executor.json
 echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${INPUT_GITHUB_RUN_NUM}\"}" >> executor.json
 cat executor.json
